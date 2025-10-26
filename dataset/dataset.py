70bfe7c7eada9a8108aa03de661fc77d2d344c1f
Philipp <philipp.selke@gmail.com>
2025-10-26 23:00:18 +0100

    Added the ASCII Art to the Welcome Screen

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,27 +1,16 @@
 import pandas as pd
 import os
+import json
 import opendatasets as od
+import sys
+
 
 #Assign the Url into a Variable
 dataset = 'https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata'
 
 #List all the Files
-os.listdir("./tmdb-movie-metadata")
+os.listdir("/home/philipp/Projects/MovieRec/dataset/tmdb-movie-metadata")
 
 #List Content of the csv Files
-movies = pd.read_csv("./tmdb-movie-metadata/tmdb_5000_movies.csv")
+movies = pd.read_csv("/home/philipp/Projects/MovieRec/dataset/tmdb-movie-metadata/tmdb_5000_movies.csv")
 
-#Getting Series out of the Dataframe to categorise them
-title = movies["title"]
-genres = movies["genres"]
-overview = movies["overview"]
-popularity = movies["popularity"]
-release_date = movies["release_date"]
-runtime = movies["runtime"]
-vote_average = movies["vote_average"]
-vote_count = movies["vote_count"]
-genres_no_na = movies[movies["genres"].notna()]
-
-#iterate over Genres because its saved in a List (shown) but saved as a string, try to access the name of the Genres
-for id, name in genres.items():
-    print(f"name: {name}")
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from movierec!")
 
+import sys
+import pyfiglet
+sys.path.append('/home/philipp/Projects/MovieRec/dataset')
+from dataset import *
+
+
+def welcome():
+    f = pyfiglet.figlet_format("Welcome", font='slant')
+    print(f)
+
+
+def main():
+    welcome()
 
 if __name__ == "__main__":
     main()
