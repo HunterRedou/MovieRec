e491b306b79c8244cc5cfe0b850d980c42d79092
Philipp <philipp.selke@gmail.com>
2025-11-10 22:37:36 +0100

    Solved the genre problem

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -14,3 +14,25 @@ os.listdir("/home/philipp/Projects/MovieRec/dataset/tmdb-movie-metadata")
 #List Content of the csv Files
 movies = pd.read_csv("/home/philipp/Projects/MovieRec/dataset/tmdb-movie-metadata/tmdb_5000_movies.csv")
 
+old_m = movies.sort_values(by="release_date")
+old = old_m.set_index("title")
+oldest_movies = old.release_date.head(5)
+
+
+
+new_m = movies.sort_values(by="release_date", ascending=False)
+new = new_m.set_index("title")
+newest_movies = new.release_date.head(5)
+
+
+print(type(movies))
+print(type(movies.genres))
+mo = movies.genres
+i = input()
+inp = str(i)
+for i, value in mo.items():
+    if inp in value:
+        print(f'Index: {i} Value: {value}')
+
+#f r genre in movies.genres:
+#    print(type(genre))
