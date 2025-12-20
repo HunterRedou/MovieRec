09c1446548cc64825ac8985478bd146e16cf02b2
Philipp <philipp.selke@gmail.com>
2025-12-21 00:38:48 +0100

    Work on the Main Function to make it run and changed the desicription

--- dataset.py
+++ dataset.py
@@ -19,29 +19,34 @@ def show_old(user_input):
     old_m = movies.sort_values(by="release_date")
     old = old_m.set_index("title")
     oldest_movies = old.release_date.head(5)
-    return oldest_movies, old
+    print(oldest_movies)
+    return old
 
 #Filter for New Movies
-def show_new(user_input):
+def show_new():
     new_m = movies.sort_values(by="release_date", ascending=False)
     new = new_m.set_index("title")
     newest_movies = new.release_date.head(5)
-    return newest_movies, new
+    print(newest_movies)
+    return new
 
 # Get Desicrition of a Movie
 
 def getting_description(user_input):
     new_ind = movies.set_index("title")
     pd.options.display.max_colwidth = 500
+    print(
+        new_ind.overview[user_input]
+    )
     return new_ind.overview[user_input]
 
 #Func to filter the Genre
 
-def getting_genres(user_input, version):
+def getting_genres(user_input):
     convert_str = str(user_input)
     found = False
     genre_list = []
-    for i, value in version.items():
+    for i, value in movies["genres"].items():
         if convert_str in value:
             genre_list.append(i)
             found = True
@@ -50,16 +55,27 @@ def getting_genres(user_input, version):
         
     
     genre_index_title = movies.loc[genre_list].set_index("title")
+    print(
+        genre_index_title.head(5)
+    )
     return genre_index_title.head(5)
 
 #Filter for the best rated Movies
-def best_in_slot(user_input, age):
-    best_m = age.sort_values(by="vote_average", ascending=False)
-    best_movies = best_m.vote_average.head(5)
+def best_in_slot():
+    best_m = movies.sort_values(by="vote_average", ascending=False)
+    best = best_m.set_index("title")
+    best_movies = best.vote_average.head(5)
+    print(
+        best_movies
+    )
     return best_movies
 
 #Filter for the length of a Movie
 def runtime_movie(user_input):
     long = movies.set_index("title")
     long_movies = long.runtime[user_input]
+    print(
+        long_movies
+    )
     return long_movies
+
--- main.py
+++ main.py
@@ -9,31 +9,42 @@ def welcome():
     welcome_ASCII = pyfiglet.figlet_format("Welcome", font='slant')
     print(welcome_ASCII)
     print("Hello, this is MovieRec your Number One Movie Recommendation Programm.\n")
-    print("In the following we ask a few Questions about what you search for, just\n")
-    print("answer the Questions with Yes or No or type the Genres what you like if\n")
-    print("the Programm ask for it. So enjoy the Questions.\n")
+    print("We have sorted out Movies to your liking with simple Commends you can acess them.\n")
+    print("The Commands that you can choose are:\n")
+    print("old - To show the oldest Movies in our Dataset.\n")
+    print("new - To show the newest Movies in our Dataset.\n")
+    print("desi - To show the desicription of a Title.\n")
+    print("genre - To show the best Movies out of that Genre.\n")
+    print("best - To show the best Movies in our Dataset.\n")
+    print("long - To show how much runtime a Movie has.\n")
+    print("welcome - To show these Commands again.\n")
+    print("end - To end the Programm.\n")
 
-def questions():
-    print("Do you like old Movies?")
-    first_input = input()
-    if first_input.lower() == "yes":
-        print("These are the oldest Movies we have to show you: \n")
-        print(oldest_movies)
-        print("To make a better Selection please type a Genres that you like. I will still be a Old Movie.")
-        user_input = input()
-        getting_genre(user_input, old_mo)
-    elif first_input.lower() == "no":
-        print("These are the newest Movies we have to show you: \n")
-        print(newest_movies)
-        print("To make a better Selection please type a Genres that you like. I will still be a New Movie.")
-        user_input = input()
-        getting_genre(user_input, new_mo)
-    else:
-        return "Sorry I think you got a typo. Only answer with Yes or No."
 
 def main():
     welcome()
-    questions()
+    run = True
+    while run:
+        user = input("Please enter a Commend from above:\n")
+        if user == "old":
+            show_old()
+        if user == "new":
+            show_new()
+        if user == "desi":
+            user_input = input("Please enter the Title that you like to see:\n")
+            getting_description(user_input)
+        if user == "genre":
+            user_input = input("Please enter the Genre that you like to see:\n")
+            getting_genres(user_input)
+        if user == "welcome":
+            welcome()
+        if user == "best":
+            best_in_slot()
+        if user == "long":
+            user_input = input("Please enter the Title that you like to see:\n")
+            runtime_movie(user_input)
+        if user == "end":
+            run = False
 
 if __name__ == "__main__":
     main()
